0ae18fc64dd0d7aea7c9ba16b238f671c3fe134b
seoLegna <49617795+seoLegna@users.noreply.github.com>
2019-12-04 11:28:51 -0500

    Got the respawn stuff working

---
+++ area_script.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+func _physics_process(delta):
+	var bodies = get_overlapping_bodies()
+	for body in bodies:
+		if body.is_in_group("Player"):
+			body.queue_free()
+			get_tree().change_scene("Game.tscn")
